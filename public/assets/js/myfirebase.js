36b1ea0d1566766e79ffc03cdd864ee630a755c9
Avinash Viswanathan <avinash.viswanathan@epsilon.com>
2020-01-15 11:45:51 +0530

    Files updated

---
+++ public/assets/js/myfirebase.js
@@ -0,0 +1,43 @@
+// firebase script
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAKSHYc8cWpCdJZb60SAKfx1RvjQFD1LkE",
+    authDomain: "ebr-timesheet.firebaseapp.com",
+    databaseURL: "https://ebr-timesheet.firebaseio.com",
+    projectId: "ebr-timesheet",
+    storageBucket: "ebr-timesheet.appspot.com",
+    messagingSenderId: "298901153823",
+    appId: "1:298901153823:web:3255fe25863cee622c4d2a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+function register() {
+
+    var userEmail = $("#userEmail").val();
+    var userPassword = $("#userPassword").val();
+
+    firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage)
+    });
+    alert("success!");
+
+}
+
+function login() {
+
+    var emailEntered = $("#emailEntered").val();
+    var passwordEntered = $("#passwordEntered").val();
+
+    firebase.auth().signInWithEmailAndPassword(emailEntered, passwordEntered).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage)
+    });
+    alert("success!");
+}
+
