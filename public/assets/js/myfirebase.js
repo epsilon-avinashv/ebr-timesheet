08a718bb10465b161383ad4eb456525781b38fff
epsilon-avinashv <46275265+epsilon-avinashv@users.noreply.github.com>
2020-01-17 00:35:40 +0530

    files updated

--- public/assets/js/myfirebase.js
+++ public/assets/js/myfirebase.js
@@ -59,16 +59,56 @@ function logout() {
 }
 
 function checkLogin() {
-    let localdata = localStorage.getItem('uid');
-    if(!localdata){
-        alert('User not Authorized');
-        window.location.href = './index.html';
-    }
+    // let localdata = localStorage.getItem('uid');
+    // if (!localdata) {
+    //     alert('User not Authorized');
+    //     window.location.href = './index.html';
+    // }
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+        } else {
+            alert('User not Authorized');
+            window.location.href = './index.html';
+        }
+    });
+}
+
+function writeUserData(empId, name, email, competency, date, brand, team, projectcode, activity, hours) {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            firebase.database().ref('associates/' + empId).set({
+                empId: empId,
+                name: name,
+                email: email,
+                competency: competency
+            });
+
+            let appendTimesheet = {
+                date: date,
+                brand: brand,
+                Team: team,
+                ProjectCode: projectcode,
+                Activity: activity,
+                Hours: hours
+            }
+
+            let ref = firebase.database().ref().child('timesheet');
+            ref.push(appendTimesheet, err => console.log(err ? 'error while pushing' : 'successful push'));
+
+        } else {
+
+        }
+    });
 }
 
 $(".add-row").click(function () {
-    var empId = '12345';
-    var name = 'Avinash';
+    var empId = '714758';
+    var name = 'Avinash Viswanathan';
+    var email = localStorage.getItem('email');
     var competency = 'FED';
     var dateEntry = $("#dateEntry").val();
     var brand = $("#brand").val();
@@ -78,8 +118,10 @@ $(".add-row").click(function () {
     var hours = $("#hours").val();
     var i = Math.floor((Math.random() * 100) + 1);
 
-    var markup = "<tr class='dynamicRow'><td><input type='checkbox' name='record' class='checkbox' onchange=chkbk('"+ i +"') id='"+ i +"'></td><td>" + empId + "</td><td>" + name + "</td><td>" + competency + "</td><td>" + dateEntry + "</td><td>" + brand + "</td><td>" + team + "</td><td>" + projectCode + "</td><td>" + activity + "</td><td>" + hours + "</td><td>&nbsp;</td></tr>";
+    var markup = "<tr class='dynamicRow'><td><input type='checkbox' name='record' class='checkbox' onchange=chkbk('" + i + "') id='" + i + "'></td><td>" + empId + "</td><td>" + name + "</td><td>" + competency + "</td><td>" + dateEntry + "</td><td>" + brand + "</td><td>" + team + "</td><td>" + projectCode + "</td><td>" + activity + "</td><td>" + hours + "</td><td>&nbsp;</td></tr>";
     $(".dynamicVal").append(markup);
+
+    writeUserData(empId, name, email, competency, dateEntry, brand, team, projectCode, activity, hours);
 });
 
 // Find and remove selected table rows
@@ -91,7 +133,7 @@ $(".delete-row").click(function () {
     });
 });
 
-function chkbk(obj){
+function chkbk(obj) {
     console.log($(obj).siblings("td"));
 }
 
